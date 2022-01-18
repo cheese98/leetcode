47b651c8a848bce8ab75ebf0c2c53b206efe55cc
cheese98 <cheesej98@gmail.com>
2022-01-18 13:10:26 -0700

    Updated on Jan 18, 2022

--- cpp/Product of Array Except Self.cpp
+++ cpp/Product of Array Except Self.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int prod = 1, zeros = 0;
-        vector<int> r;
-        for (int num : nums) {
-            if (num != 0) prod *= num;
-            else zeros++;
-        }
-        if (zeros > 1) {
-            for(int i = 0; i < nums.size(); i++) r.push_back(0);
-        }
-        else if (zeros == 1) {
-            for(int i = 0; i < nums.size(); i++) {
-                if (nums[i] == 0) r.push_back(prod);
-                else r.push_back(0);
-            }
-        }
-        else {
-            for (int num : nums) r.push_back(prod / num);
-        }
-        return r;
-    }
+		vector<int> res;
+		int left = 1, right = 1;
+		for (int i = 0; i < nums.size(); i++) res.push_back(1);
+		for (int i = 0; i < nums.size(); i++) {
+			res[i] *= left;
+			left *= nums[i];
+			res[nums.size() - 1 - i] *= right;
+			right *= nums[nums.size() - 1 - i];
+		}
+		return res;
+	}
 };
